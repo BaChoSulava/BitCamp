d05497c3e388957a4cce4aa6ddbf06425cc866c0
BaCho <bachana.sulava@gmail.com>
2023-03-24 11:10:58 +0400

    test file done

---
+++ #13/test_camel.py
@@ -0,0 +1,6 @@
+from camel import convert_to_snake
+
+def test_convert_to_snake():
+    assert convert_to_snake("name") == "name"
+    assert convert_to_snake("firstName") == "first_name"
+    assert convert_to_snake("preferredFirstName") == "preferred_first_name"
