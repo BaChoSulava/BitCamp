1a8496deb9029b2f05407c8699abaeff3242003a
BaCho <bachana.sulava@gmail.com>
2023-03-24 13:43:08 +0400

    test file done

---
+++ #14/test_coke.py
@@ -0,0 +1,12 @@
+from coke import calculations
+
+def calculations_test():
+    assert calculations(25) == "Amount Due: 25"
+    assert calculations(10) == "Amount Due: 40"
+    assert calculations(5) == "Amount Due: 45"
+    assert calculations(30) == "Amount Due: 50"
+    assert calculations(50) == "Change Owed: 0"
+    assert calculations(60) == "Change Owed: 10"
+
+
+calculations_test()
