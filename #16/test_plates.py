f8ae5396e84f2e49a9e7703b097eef16ec4f78d6
BaCho <bachana.sulava@gmail.com>
2023-03-27 17:23:24 +0400

    done

--- #16/test_plates.py
+++ #16/test_plates.py
@@ -1,9 +1,11 @@
+
 from plates import is_valid
 
+
 def test_is_valid():
-    assert is_valid("CS50") == "Valid"
-    assert is_valid("CS05") == "Invalid"
-    assert is_valid("CS50P") == "Invalid"
-    assert is_valid("PI3.14") == "Invalid"
-    assert is_valid("H") == "Invalid"
-    assert is_valid("OUTATIME") == "Invalid"
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == True
+    assert is_valid("CS50P") == False
+    assert is_valid("PI3.14") == False
+    assert is_valid("H") == False
+    assert is_valid("OUTATIME") == False
