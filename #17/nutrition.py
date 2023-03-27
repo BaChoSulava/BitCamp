90285e0ffb83d6d566ef3b60c8ca153dfc61dd03
BaCho <bachana.sulava@gmail.com>
2023-03-27 18:10:47 +0400

    done nutrition.py

---
+++ #17/nutrition.py
@@ -0,0 +1,32 @@
+def main():
+    user_input = input("Item: ")
+    check_for_calories(user_input)
+
+def check_for_calories(user_input):
+    fruits = {
+        "Apple" : 130,
+        "Avocado" : 50,
+        "Banana" : 110,
+        "Cantaloupe" : 50,
+        "Grapefruit" : 60,
+        "Grapes" : 90,
+        "Honeydew Melon" : 50,
+        "Kiwifruit" : 90,
+        "Lemon" : 15,
+        "Lime" : 20,
+        "Nectarine" : 60,
+        "Orange" : 80,
+        "Peach" : 60,
+        "Pear" : 60,
+        "Pineapple" : 50,
+        "Plums" : 70,
+        "Strawberries" : 50,
+        "Sweet Cherries" : 100,
+        "Tangerine" : 50,
+        "Watermelon" : 80
+    }
+    if user_input in fruits:
+        return fruits[user_input]
+
+if __name__ == "__main__":
+    main()
