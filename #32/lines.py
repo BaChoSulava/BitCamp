89e8d23c0e510ae6387089ddc367891a7ef1fc79
BaCho <bachana.sulava@gmail.com>
2023-04-02 14:10:11 +0400

    done

--- #32/lines.py
+++ #32/lines.py
@@ -3,15 +3,19 @@ import sys
 #import file name
 path = input("file's name or path: ")
 
+print(sys.argv)
+
 #checks for .py ending
-if not path.endswith(".py"):
-    sys.exit("FileNotFoundError")
+if not path.endswith(".py") and len(sys.argv) != 1:
+    sys.exit("File doesn't exist or too much arguments is written")
 
 #counts lines
-count = 0
-with open(path, "r") as file:
-    for each_line in file:
-        if not each_line.startswith("#") and not each_line.isspace():
-            count += 1
-    print("There is", count, "line code")
-    
+try:
+    count = 0
+    with open(path, "r") as file:
+        for each_line in file:
+            if not each_line.startswith("#") and not each_line.isspace():
+                count += 1
+        print("There is", count, "line code in the", path)
+except FileNotFoundError:
+    sys.exit("FileNotFoundError")
