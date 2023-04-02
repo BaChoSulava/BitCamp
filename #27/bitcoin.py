05e5a44db88720ed3a7ff843df139b0b582b8a9e
BaCho <bachana.sulava@gmail.com>
2023-04-02 18:01:41 +0400

    done

--- #27/bitcoin.py
+++ #27/bitcoin.py
@@ -1,14 +1,31 @@
 import requests
 import sys
 
+#check for correct command-line inputs
+if len(sys.argv) < 2:
+        sys.exit("Missing command-line argument")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
 
+
+#check for float numbering
 try:
-    user_bitcoin = float(input("How many to buy? "))
-except requests.RequestException:
-    ...
+    bitcoins = float(sys.argv[1])
 except ValueError:
     sys.exit("Error: Invalid number of bitcoins")
 
-url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-print(f"${user_bitcoin:,.4f}")
+#take data from url
+try:
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    response.raise_for_status()
+    data = response.json()
+    # print(data)
+    price = data["bpi"]["USD"]["rate_float"]
+    # print(price)
+except requests.RequestException:
+    sys.exit("Error: could not retrieve bitcoin price")
+
+# Calculate
+cost = bitcoins * price
+print(f"${cost:,.4f}")
