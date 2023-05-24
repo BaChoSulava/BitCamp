1f09f1c3e5e7987ccd7244e1ce076ff4e726c35a
BaCho <bachana.sulava@gmail.com>
2023-05-24 11:22:18 +0400

    added files

--- final_project/project.py
+++ final_project/project.py
@@ -1,14 +1,41 @@
+import datetime
+from datetime import date, datetime, timedelta
+import sys
+import inflect
+
+
 def main():
-    ...
+    birthday = get_date()
+    answer = age_in_minutes(birthday)
+    
+    if answer != 1:
+        answer += "s"
+    print(answer)
+    
 
-def func1():
-    ...
+def get_date():
+    birth_date = input("Date of Birth: ")
+    formated_date = birth_date.strip().split("-")
+    try:
+        for i in formated_date:
+            i.isdigit()
+    except:
+        sys.exit("Invalid date")
+    
+    if len(formated_date[0]) != 4 and len(formated_date[1]) != 2 and len(formated_date[2]) != 2:
+        sys.exit("Invalid date")
+    return formated_date
 
-def func2():
-    ...
+def age_in_minutes(birthday):
+    user_birthdate = date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
 
-def func3():
-    ...
+    today = date.today()
+    age = today - user_birthdate
+    age_in_minutes = round(age.total_seconds() / 60)
+
+    p = inflect.engine()
+    age_str = p.number_to_words(age_in_minutes)
+    return age_str
 
 
 if __name__ == '__main__':
--- test.py
+++
@@ -1,2 +0,0 @@
-empty = input("write your email: ")
-
